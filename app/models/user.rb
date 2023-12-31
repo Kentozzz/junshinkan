adc08ab0836ff7d65ee1647f29129f78d14ac6e7
kentozzz <kento.sobayashi@di-v.co.jp>
2024-01-01 07:52:35 +0900

    1番の管理者を削除できないようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_destroy :prevent_destroy_admin
+
+  def prevent_destroy_admin
+    if self.id == 1
+      errors.add(:base, "このユーザーは削除できません。")
+      throw :abort
+    end
+  end
+
 end
